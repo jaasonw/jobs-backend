5a31de4bf86f7b42453591f0be449d8763c0c71f
jason wong <jayywong92@gmail.com>
2023-10-27 16:34:37 -0700

    migrate to new table, add stats endpoint

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import json
 from shillelagh.backends.apsw.db import connect
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
@@ -6,18 +7,6 @@ import csv
 
 app = FastAPI()
 
-@app.get("/api/python")
-def test():
-    connection = connect(":memory:")
-    cursor = connection.cursor()
-
-    SQL = """
-    SELECT Company
-    FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=0"
-    """
-    rows = [row for row in cursor.execute(SQL)]
-    return {"rows": rows}
-
 @app.get("/api/jobs", response_class=PlainTextResponse)
 def get_jobs():
     connection = connect(":memory:")
@@ -25,7 +14,8 @@ def get_jobs():
 
     SQL = """
     SELECT Date, URL, Company, Title, Location, Technologies, "Years of Experience"
-    FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=0"
+    FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=1442393429"
+    WHERE "Years of Experience" < 3 AND Blacklist != 'True' AND TS_SCI != 'True' AND Mid != 'True' AND "Senior" != 'True'
     """
     rows = [row for row in cursor.execute(SQL)]
     field_names = [i[0] for i in cursor.description]
@@ -34,3 +24,34 @@ def get_jobs():
     writer.writerow(field_names)
     writer.writerows(rows)
     return output.getvalue().replace('\r\n', '\n').encode('utf-8')
+
+@app.get("/api/stats", response_class=PlainTextResponse)
+def get_stats():
+    connection = connect(":memory:")
+    cursor = connection.cursor()
+
+    job_db = "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=1442393429"
+    metadata_db = "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=2060895389"
+    SQL = f"""
+    SELECT
+        l.last_updated,
+        counts.*
+    FROM (
+        SELECT
+            COUNT(*) as total_count,
+            SUM(Senior) as senior_count,
+            SUM(Mid) as mid_count,
+            SUM(TS_SCI) as ts_sci_count,
+            SUM(Intern) as intern_count,
+            SUM(Blacklist) as blacklist_count
+        FROM "{job_db}"
+    ) AS counts
+    CROSS JOIN (
+        SELECT last_updated
+        FROM "{metadata_db}"
+    ) AS l
+    """
+    rows = [row for row in cursor.execute(SQL)]
+    field_names = [i[0] for i in cursor.description]
+    data_dict = {field_names[i]: rows[0][i] for i in range(len(field_names))}
+    return json.dumps(data_dict)
