6e0664e18f68689c07aec01e3797cad23a0365ba
jason wong <jayywong92@gmail.com>
2023-10-27 22:42:20 -0700

    dynamically query for blacklist companies

--- api/index.py
+++ api/index.py
@@ -7,23 +7,37 @@ import csv
 
 app = FastAPI()
 
+
 @app.get("/api/jobs", response_class=PlainTextResponse)
 def get_jobs():
     connection = connect(":memory:")
     cursor = connection.cursor()
 
+    # for some reason directly joining causes the query to hang, download the table locally instead
+    create_blacklist_table = """
+        CREATE TABLE IF NOT EXISTS blacklist as SELECT * FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=206068366"
+    """
+    cursor.execute(create_blacklist_table)
+
     SQL = """
-    SELECT Date, URL, Company, Title, Location, Technologies, "Years of Experience"
-    FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=1442393429"
-    WHERE "Years of Experience" < 3 AND Blacklist != 'True' AND TS_SCI != 'True' AND Mid != 'True' AND "Senior" != 'True'
+    SELECT Date, URL, jobs.Company as Company, Title, Location, Technologies, "Years of Experience"
+    FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=1442393429" AS jobs
+    LEFT JOIN blacklist
+    ON UPPER(blacklist.Company) LIKE UPPER(jobs.Company)
+    WHERE blacklist.Company IS NULL
+        AND "Years of Experience" < 3
+        AND TS_SCI != 'True'
+        AND Mid != 'True'
+        AND "Senior" != 'True'
     """
     rows = [row for row in cursor.execute(SQL)]
     field_names = [i[0] for i in cursor.description]
-    output = io.StringIO(newline='\n')
+    output = io.StringIO(newline="\n")
     writer = csv.writer(output)
     writer.writerow(field_names)
     writer.writerows(rows)
-    return output.getvalue().replace('\r\n', '\n').encode('utf-8')
+    return output.getvalue().replace("\r\n", "\n").encode("utf-8").rstrip()
+
 
 @app.get("/api/stats", response_class=PlainTextResponse)
 def get_stats():
