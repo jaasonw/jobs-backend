aa7cdfafbda5688b4933b1fb9072812cea5580ba
jason wong <jayywong92@gmail.com>
2023-11-15 19:35:36 -0800

    add internship endpoint without testing it

--- api/index.py
+++ api/index.py
@@ -69,3 +69,31 @@ def get_stats():
     field_names = [i[0] for i in cursor.description]
     data_dict = {field_names[i]: rows[0][i] for i in range(len(field_names))}
     return json.dumps(data_dict)
+
+
+@app.get("/api/internships", response_class=PlainTextResponse)
+def get_internships():
+    connection = connect(":memory:")
+    cursor = connection.cursor()
+
+    # for some reason directly joining causes the query to hang, download the table locally instead
+    create_blacklist_table = """
+        CREATE TABLE IF NOT EXISTS blacklist as SELECT * FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=206068366"
+    """
+    cursor.execute(create_blacklist_table)
+
+    SQL = """
+    SELECT Date, URL, jobs.Company as Company, Title, Location, Technologies, "Years of Experience"
+    FROM "https://docs.google.com/spreadsheets/d/1Rp1yFWi6yJMhUGq2fkfGUhkmteScma5r9XkUEbqhq14/edit#gid=1442393429" AS jobs
+    LEFT JOIN blacklist
+    ON UPPER(blacklist.Company) LIKE UPPER(jobs.Company)
+    WHERE blacklist.Company IS NULL
+        AND Intern = 'True'
+    """
+    rows = [row for row in cursor.execute(SQL)]
+    field_names = [i[0] for i in cursor.description]
+    output = io.StringIO(newline="\n")
+    writer = csv.writer(output)
+    writer.writerow(field_names)
+    writer.writerows(rows)
+    return output.getvalue().replace("\r\n", "\n").encode("utf-8").rstrip()
